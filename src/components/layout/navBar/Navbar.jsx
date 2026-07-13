bec00bed70d68a0b4f2862a25fbe26b1de768460
Daniel Mazzola <mazzoladaniele@gmail.com>
2026-07-13 13:19:40 +0200

    updates NAVBAR - style

--- src/components/layout/navBar/Navbar.jsx
+++ src/components/layout/navBar/Navbar.jsx
@@ -19,14 +19,15 @@ function Navbar() {
       <div>
         <div className="logo link_color">
           <Link to={navbar[0].path}>{navbar[0].title}</Link>
+          <div className="btn_toogle_menu" onClick={handleToogleMenu}>
+            <p>MENU</p>
+          </div>
         </div>
 
         <div className="link_color">
-          <div className="btn_toogle_menu" onClick={handleToogleMenu}>
-            MENU
-          </div>
-
-          <div className={`links ${toogleMenu ? "show_menu_toogle" : ""}`}>
+          <div
+            className={`links ${toogleMenu ? "show_menu_toogle filter fadeIn" : ""}`}
+          >
             {navbar
               .filter((item) => item.id !== 1)
               .map((item) => (
