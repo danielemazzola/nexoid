90350391199a6d9f1a00fe4eb0cd9bc75629edf4
Daniel Mazzola <mazzoladaniele@gmail.com>
2026-07-13 12:20:57 +0200

    updates NAVBAR

--- src/components/layout/navBar/Navbar.jsx
+++ src/components/layout/navBar/Navbar.jsx
@@ -1,24 +1,45 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import navbar from "../../../data/navbar.js";
 import "./Navbar.css";
 
 function Navbar() {
+  const [toogleMenu, setToogleMenu] = useState(false);
+
+  const handleToogleMenu = () => {
+    setToogleMenu((prev) => !prev);
+  };
+
+  useEffect(() => {
+    console.log(toogleMenu);
+  }, [toogleMenu]);
+
   return (
     <nav className="filter">
       <div>
         <div className="logo link_color">
-          <Link key={navbar[0].id} to={navbar[0].path}>
-            {navbar[0].title}
-          </Link>
+          <Link to={navbar[0].path}>{navbar[0].title}</Link>
         </div>
-        <div className="links link_color">
-          {navbar.map((item, index) =>
-            item.id === 1 ? null : (
-              <Link key={item.id} to={item.path}>
-                {item.title}
-              </Link>
-            ),
-          )}
+
+        <div className="link_color">
+          <div className="btn_toogle_menu" onClick={handleToogleMenu}>
+            MENU
+          </div>
+
+          <div className={`links ${toogleMenu ? "show_menu_toogle" : ""}`}>
+            {navbar
+              .filter((item) => item.id !== 1)
+              .map((item) => (
+                <Link
+                  className="show_menu"
+                  key={item.id}
+                  to={item.path}
+                  onClick={() => setToogleMenu(false)}
+                >
+                  {item.title}
+                </Link>
+              ))}
+          </div>
         </div>
       </div>
     </nav>
